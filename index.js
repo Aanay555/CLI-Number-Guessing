3ed64c0f7f791e7c4fa320ce037544df2d52243f
Aanay555 <160274256+Aanay555@users.noreply.github.com>
2024-03-28 19:47:10 +0500

    js file

---
+++ index.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+// generate computer random number
+//take input from user
+// after taking input campare user input and computer generated guess numbers
+const RandomNumber = Math.floor(Math.random() * 6 + 1);
+console.log(chalk.blueBright(RandomNumber));
+const result = await inquirer.prompt([
+    {
+        type: "number",
+        name: "GuessUserNumber",
+        message: "Please guess your number."
+    },
+]);
+if (result.GuessUserNumber === RandomNumber) {
+    console.log(chalk.bgRed("Great : You are right, for guesssing number."));
+}
+else {
+    console.log(chalk.bgRed("Sorry : You are wrong, for guessing number."));
+}
